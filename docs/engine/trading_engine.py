8904f2249c71b077b6f0dee39f158ea093849c65
Groootrade <hello.groootrade@gmail.com>
2026-06-23 22:48:03 +0200

    Add TradingEngine class with basic methods
    Add TradingEngine

---
+++ docs/engine/trading_engine.py
@@ -0,0 +1,10 @@
+class TradingEngine:
+
+    def start(self):
+        print("GroooTrade AI started.")
+
+    def analyze_market(self):
+        print("Analyzing market...")
+
+    def request_decision(self):
+        print("Requesting decision from brains...")
