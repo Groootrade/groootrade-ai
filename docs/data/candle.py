e0345b624a434d42bea8eb4c3bd3332aaa48223b
Groootrade <hello.groootrade@gmail.com>
2026-06-23 22:52:27 +0200

    Add Candle class for price data representation
    Add Candle class

---
+++ docs/data/candle.py
@@ -0,0 +1,7 @@
+class Candle:
+
+    def __init__(self, open_price, high_price, low_price, close_price):
+        self.open_price = open_price
+        self.high_price = high_price
+        self.low_price = low_price
+        self.close_price = close_price
