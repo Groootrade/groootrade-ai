6730d57c122ab4c80c311fa828987aad6b80404f
Groootrade <hello.groootrade@gmail.com>
2026-06-24 23:17:04 +0200

    Add SymbolRegistry class with predefined symbols
    Add SymbolRegistry

---
+++ docs/market/symbol_registry.py
@@ -0,0 +1,18 @@
+class SymbolRegistry:
+
+    SYMBOLS = [
+        "XAUUSD",
+        "XAGUSD",
+        "EURUSD",
+        "GBPUSD",
+        "USDJPY",
+        "AUDUSD",
+        "BTCUSD",
+        "ETHUSD",
+        "NAS100",
+        "US30",
+        "SPX500",
+        "DAX40",
+        "WTI",
+        "BRENT"
+    ]
