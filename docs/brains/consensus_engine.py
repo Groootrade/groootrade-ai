a4667944a19ebc9533eb1e97c5d50bdd831c4beb
Groootrade <hello.groootrade@gmail.com>
2026-06-24 23:32:49 +0200

    Refactor calculate_consensus to handle votes
    Upgrade ConsensusEngine

--- docs/brains/consensus_engine.py
+++ docs/brains/consensus_engine.py
@@ -2,3 +2,17 @@ class ConsensusEngine:
 
     def calculate_consensus(self):
         print("Calculating consensus...")
+
+class ConsensusEngine:
+
+    def calculate_consensus(self, votes):
+
+        buy_votes = votes.count("BUY")
+        sell_votes = votes.count("SELL")
+        wait_votes = votes.count("WAIT")
+
+        print(f"BUY: {buy_votes}")
+        print(f"SELL: {sell_votes}")
+        print(f"WAIT: {wait_votes}")
+
+        return buy_votes, sell_votes, wait_votes
