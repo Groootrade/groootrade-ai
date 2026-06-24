ff387b3029ede4be2448cf76a149e1cf1cf58b43
Groootrade <hello.groootrade@gmail.com>
2026-06-24 23:06:33 +0200

    Add MacroGuardian class with event checking and approval
    Add MacroGuardian

---
+++ docs/guardian/macro_guardian.py
@@ -0,0 +1,7 @@
+class MacroGuardian:
+
+    def check_events(self):
+        print("Checking high impact economic events...")
+
+    def approve_trade(self):
+        print("Macro conditions validated.")
