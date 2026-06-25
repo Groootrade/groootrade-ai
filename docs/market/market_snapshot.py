404de510340a47c4d5b37b0e6b348a9e96f2c204
Groootrade <hello.groootrade@gmail.com>
2026-06-25 22:25:07 +0200

    Add MarketSnapshot class for market data representation

--- docs/market/market_snapshot.py
+++ docs/market/market_snapshot.py
@@ -11,3 +11,20 @@ class MarketSnapshot:
         self.spread = 0.20
 
         self.timeframe = "M15"
+
+class MarketSnapshot:
+
+    def __init__(
+        self,
+        symbol,
+        price,
+        volume,
+        spread,
+        timeframe
+    ):
+
+        self.symbol = symbol
+        self.price = price
+        self.volume = volume
+        self.spread = spread
+        self.timeframe = timeframe
