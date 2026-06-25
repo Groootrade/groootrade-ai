8f39883e59cdbd09e6df327971ccb7b50596e02a
Groootrade <hello.groootrade@gmail.com>
2026-06-25 23:06:58 +0200

    Add ServiceContainer class for service management
    Create ServiceContainer

---
+++ docs/core/service_container.py
@@ -0,0 +1,13 @@
+class ServiceContainer:
+
+    def __init__(self):
+
+        self._services = {}
+
+    def register(self, name, service):
+
+        self._services[name] = service
+
+    def get(self, name):
+
+        return self._services.get(name)
