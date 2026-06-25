e380d6f2f52f8e41eaa548e6e2d812208ceb28d1
Groootrade <hello.groootrade@gmail.com>
2026-06-25 22:19:38 +0200

    Add GroooPhilosophy class with guiding messages
    Add GroooPhilosophy

---
+++ docs/dna/philosophy.py
@@ -0,0 +1,10 @@
+class GroooPhilosophy:
+
+    def message(self):
+
+        return (
+            "Protect before performance.\n"
+            "Explain before advising.\n"
+            "Discipline before emotion.\n"
+            "Learn before earning."
+        )
