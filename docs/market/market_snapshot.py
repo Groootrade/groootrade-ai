4700a25baa4e1703630bc95a36c09721425888d9
Groootrade <hello.groootrade@gmail.com>
2026-06-25 22:08:40 +0200

    Add initialization to MarketSnapshot class
    Add MarketSnapshot model

--- docs/market/market_snapshot.py
+++ docs/market/market_snapshot.py
@@ -1,4 +1,13 @@
 class MarketSnapshot:
 
-    def capture(self):
-        print("Capturing market state...")
+    def __init__(self):
+
+        self.symbol = "XAUUSD"
+
+        self.price = 3350.25
+
+        self.volume = 18542
+
+        self.spread = 0.20
+
+        self.timeframe = "M15"
